9ad23e014e25ee63443bb2c50a03ee4b526fef62
canhhocit <canhhuu26@gmail.com>
2026-02-06 22:13:35 +0700

    push

--- test01/src/main/java/com/example/test01/models/User.java
+++ test01/src/main/java/com/example/test01/models/User.java
@@ -19,7 +19,7 @@ import lombok.ToString;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private int age;
